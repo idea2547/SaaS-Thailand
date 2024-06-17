f52fce011c54d701bb887f0aa913c09e4f8ec237
Idea Phakpaphon <ideapakpaphon@gmail.com>
2024-06-17 17:52:02 +0700

    Add iconify

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
     import { Button } from "$lib/components/ui/button";
+    import Icon from '@iconify/svelte';
 </script>
 
 
-<div class="flex flex-col w-full h-screen items-center justify-center">
+<div class="flex flex-col w-full h-screen items-center justify-center p-5">
    
-    <h1 class="text-5xl mb-4"> SaaS Thailand </h1>
-    <a href="https://web.facebook.com/profile.php?id=61560121365380">
+    <h1 class="text-5xl p-4"> SaaS Thailand </h1>
+    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfY2ougjKrJ5aRfpSqEY59aq9_7scY1o8Jtf5f597wdBsbmnw/viewform?usp=sf_link">
         <Button>เรียนรู้เพิ่มเติม</Button>
     </a>
+    <!-- grid-cols-2 gap-4 p-3 lg:grid-cols-3 -->
+    <div class="grid p-3">
+        <a href="https://web.facebook.com/profile.php?id=61560121365380">
+            <Icon icon="ic:baseline-facebook" style="font-size: 2.5rem;" />
+        </a>
+        
+    </div>
+    
     
 </div>
